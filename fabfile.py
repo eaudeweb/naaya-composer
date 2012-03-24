15280d13cc2588308bff0ae21bf0045a9347bb5e
Alex Morega <alex@grep.ro>
2012-03-24 13:56:54 +0200

    bootstrap a buildout environment

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import env
+from fabric.api import env, cd
 from fabric.operations import open_shell, run, sudo
 
 
@@ -17,3 +17,14 @@ def bootstrap():
     run('virtualenv -p python2.6 '
         '--no-site-packages --distribute '
         '/var/local/naaya')
+    sudo('apt-get install curl -y')
+    with cd('/var/local/naaya'):
+        for name in ['bootstrap.py',
+                     'buildout.cfg',
+                     'naaya.cfg',
+                     'versions-zope-2.12.18.cfg',
+                     'versions.cfg']:
+            url = ('https://svn.eionet.europa.eu/repositories/Naaya'
+                   '/buildout/Naaya/zope212/' + name)
+            run('curl -O ' + url)
+        run('bin/python bootstrap.py -d')
