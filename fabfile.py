866edfa27ed80aab9c1a7279a19b9d9787b0746c
Alex Morega <alex@grep.ro>
2012-03-24 13:50:49 +0200

    bootstrap a virtualenv

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from fabric.api import env
-from fabric.operations import open_shell
+from fabric.operations import open_shell, run, sudo
 
 
 env.use_ssh_config = True
@@ -8,3 +8,12 @@ env.hosts = ['vagrant@192.168.13.29']
 
 def shell():
     open_shell()
+
+
+def bootstrap():
+    sudo('apt-get install python-virtualenv -y')
+    sudo('mkdir /var/local/naaya')
+    sudo('chown vagrant: /var/local/naaya')
+    run('virtualenv -p python2.6 '
+        '--no-site-packages --distribute '
+        '/var/local/naaya')
