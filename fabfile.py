836f3b6cbbbc47bc02502e63ca41d7ca32177152
Alex Morega <alex@grep.ro>
2012-03-26 01:06:04 +0300

    explicitly install python2.6-dev

--- fabfile.py
+++ fabfile.py
@@ -13,7 +13,7 @@ def shell():
 
 @task
 def bootstrap():
-    sudo('apt-get install -y python-dev python-virtualenv curl')
+    sudo('apt-get install -y python2.6-dev python-virtualenv curl')
     sudo('mkdir /var/local/naaya')
     sudo('chown vagrant: /var/local/naaya')
     run('virtualenv -p python2.6 '
