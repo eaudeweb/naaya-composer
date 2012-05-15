d56c5863da63134195834832a7240605ac4feeac
Alex Morega <alex@grep.ro>
2012-05-15 16:48:43 +0300

    demo deployment of Reportek (zope 2.13) on martini

---
+++ martini-cdr-zope213/fabfile.py
@@ -0,0 +1,55 @@
+from fabric.api import *
+from path import path
+from fabric.contrib.files import exists
+
+
+fabdir = path(__file__).abspath().dirname()
+app = env.app = {
+    'fabdir': fabdir,
+    'buildout-path': path('/var/local/cdr-zope213'),
+    'reportek-repo': 'https://github.com/eaudeweb/Products.Reportek.git',
+    'reportek-branch': 'alexm-zope213',
+}
+
+env['hosts'] = ['edw@martini.edw.ro']
+
+
+def _virtualenv(venv_path, python_bin='python2.6'):
+    run("virtualenv %s --no-site-packages --distribute --python=%s" %
+        (venv_path, python_bin))
+
+
+def _git_repo(repo_path, origin_url, update=True, branch='master'):
+    if not exists(repo_path/'.git'):
+        run("mkdir -p '%s'" % repo_path)
+        with cd(repo_path):
+            run("git clone '%s' ." % origin_url)
+            run("git checkout %s" % branch)
+
+    elif update:
+        with cd(repo_path):
+            run("git fetch origin")
+            run("git merge origin/%s --ff-only" % branch)
+
+
+@task
+def ssh():
+    open_shell("cd '%(buildout-path)s'" % app)
+
+
+@task
+def deploy():
+    if not exists(app['buildout-path']/'bin'/'python'):
+        _virtualenv(app['buildout-path'])
+    _git_repo(app['buildout-path']/'src'/'Products.Reportek',
+              app['reportek-repo'],
+              branch=app['reportek-branch'])
+    with cd(app['buildout-path']):
+        paths = put('%(fabdir)s/buildout/*' % app, '.')
+        run("'%(buildout-path)s/bin/python' bootstrap.py -d" % app)
+        run("bin/buildout")
+    if not exists(app['buildout-path']/'products'):
+        run("mkdir '%(buildout-path)s/products'" % app)
+        with cd(app['buildout-path']/'products'):
+            url = "http://eggshop.eaudeweb.ro/LDAPUserFolder-cdr2.tgz"
+            run("curl '%s' | tar xzf -" % url)
