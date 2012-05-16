7c00f689e065c21607a6a876fd0988561cc4488c
Mihnea Simian <contact@mesimian.com>
2012-05-16 10:30:34 +0300

    added jenkins-docs-reportek; make-docs works

---
+++ jenkins-docs-reportek/fabfile.py
@@ -0,0 +1,47 @@
+from fabric.api import *
+from path import path
+from fabric.contrib.files import exists
+
+
+fabdir = path(__file__).abspath().dirname()
+app = env.app = {
+    'fabdir': fabdir,
+    'buildout-path': path('/var/lib/jenkins/jobs/Reportek-docs-zope210/workspace'),
+    'reportek-repo': 'https://svn.eionet.europa.eu/repositories/Zope/trunk/Products.Reportek/',
+}
+
+env['hosts'] = ['edw@power.edw.ro']
+
+
+def _virtualenv(venv_path, python_bin='python2.6'):
+    run("virtualenv %s --no-site-packages --distribute --python=%s" %
+        (venv_path, python_bin))
+
+
+def _svn_repo(repo_path, origin_url, update=True):
+    if not exists(repo_path/'.svn'):
+        run("mkdir -p '%s'" % repo_path)
+        with cd(repo_path):
+            run("svn co '%s' ." % origin_url)
+
+    elif update:
+        with cd(repo_path):
+            run("svn up")
+
+
+@task
+def ssh():
+    open_shell("cd '%(buildout-path)s'" % app)
+
+
+@task
+def deploy():
+    if not exists(app['buildout-path']/'bin'/'python'):
+        _virtualenv(app['buildout-path'], python_bin='python2.4')
+    _svn_repo(app['buildout-path']/'src'/'Products.Reportek',
+              app['reportek-repo'])
+    with cd(app['buildout-path']):
+        paths = put('%(fabdir)s/buildout/*' % app, '.')
+        run("'%(buildout-path)s/bin/python' bootstrap.py -d" % app)
+        run("bin/buildout")
+
