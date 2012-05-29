1325c85a8dcfc85ad89f96fa68cd5f3d072e8ea1
Alex Morega <alex@grep.ro>
2012-05-29 18:37:54 +0300

    update jenkins reportek tests to zope 2.13

--- jenkins-tests-reportek/fabfile.py
+++ jenkins-tests-reportek/fabfile.py
@@ -7,7 +7,7 @@ fabdir = path(__file__).abspath().dirname()
 svn_repo = 'https://svn.eionet.europa.eu/repositories/Zope'
 app = env.app = {
     'fabdir': fabdir,
-    'buildout-path': path('/var/lib/jenkins/jobs/Reportek-tests-zope210/workspace'),
+    'buildout-path': path('/var/lib/jenkins/jobs/Reportek-tests-zope213/workspace'),
     'reportek-repo': svn_repo + '/trunk/Products.Reportek',
 }
 
@@ -17,19 +17,6 @@ env['hosts'] = ['edw@power.edw.ro']
 def _virtualenv(venv_path, python_bin='python2.6'):
     run("virtualenv %s --no-site-packages --distribute --python=%s" %
         (venv_path, python_bin))
-
-
-def _git_repo(repo_path, origin_url, update=True, branch='master'):
-    if not exists(repo_path/'.git'):
-        run("mkdir -p '%s'" % repo_path)
-        with cd(repo_path):
-            run("git clone '%s' ." % origin_url)
-            run("git checkout %s" % branch)
-
-    elif update:
-        with cd(repo_path):
-            run("git fetch origin")
-            run("git merge origin/%s --ff-only" % branch)
 
 
 def _svn_repo(repo_path, origin_url, update=True):
@@ -51,10 +38,12 @@ def ssh():
 @task
 def deploy():
     if not exists(app['buildout-path']/'bin'/'python'):
-        _virtualenv(app['buildout-path'], python_bin='python2.4')
+        _virtualenv(app['buildout-path'])
     _svn_repo(app['buildout-path']/'src'/'Products.Reportek',
-              app['reportek-repo'])
+              app['reportek-repo'],
+              update=False)
     with cd(app['buildout-path']):
-        paths = put('%(fabdir)s/buildout/*' % app, '.')
+        put('%(fabdir)s/buildout/*' % app, '.')
+        run("curl -O 'http://eggshop.eaudeweb.ro/bootstrap.py'")
         run("'%(buildout-path)s/bin/python' bootstrap.py -d" % app)
         run("bin/buildout")
