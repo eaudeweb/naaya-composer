8d72557530679262641d724184ff8692e87d2b95
Mihnea Simian <contact@mesimian.com>
2012-08-07 16:24:53 +0300

    added edw.circaimport, destinet tests

--- jenkins-tests-naaya-additional/fabfile.py
+++ jenkins-tests-naaya-additional/fabfile.py
@@ -9,6 +9,7 @@ app = env.app = {
     'fabdir': fabdir,
     'buildout-path': ppath('/var/lib/jenkins/jobs/Naaya-tests-naaya-additional/workspace'),
     'trunk-eggs-repo': 'https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/',
+    'bundles-repos': ['https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/NaayaBundles-DESTINET/', ]
 }
 
 env['hosts'] = ['edw@power.edw.ro']
@@ -40,6 +41,9 @@ def deploy():
     if not exists(app['buildout-path']/'bin'/'python'):
         _virtualenv(app['buildout-path'])
     _svn_repo(app['buildout-path']/'src'/'trunk-eggs', app['trunk-eggs-repo'])
+    for bundle_repo in app['bundles-repos']:
+        bundle_package = bundle_repo.strip('/').rsplit('/', 1)[-1]
+        _svn_repo(app['buildout-path']/'src'/'bundles'/bundle_package, bundle_repo)
     with cd(app['buildout-path']):
         paths = put('%(fabdir)s/buildout/*' % app, '.')
         run("'%(buildout-path)s/bin/python' bootstrap.py -d" % app)
